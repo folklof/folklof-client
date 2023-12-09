915832f445bc4a440499efd1f1673b3e8d3cfa03
sigitwie <wirsigit@gmail.com>
2023-12-09 16:08:34 +0700

    [HAN-50] feat: add main dashboard page

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -3,19 +3,16 @@ import { TopNavbar, DashboardNavbar, Carousel, Footer } from "../../components";
 import { BestStories, NewRelease } from "../../containers";
 
 const DashboardPage: React.FC = () => {
-
-    return (
-        <>
-        
-        <TopNavbar/>
-        <DashboardNavbar/>
-        <Carousel/>
-        <BestStories/>
-        <NewRelease/>
-        <Footer/>
-        </>
-        
-    );
+  return (
+    <>
+      <TopNavbar />
+      <DashboardNavbar />
+      <Carousel />
+      <BestStories />
+      <NewRelease />
+      <Footer />
+    </>
+  );
 };
 
 export default DashboardPage;
